15c90ee5282327304c9b4978a0febf130f3532b0
faith993 <chepkemoitoo3@gmail.com>
2025-03-31 22:09:23 +0300

    vite base path added

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     'process.env': process.env
   },
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/cosmetic_formula_frontend",
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
